e3dca47fbcf80f042d49d58c0d1608e39117877c
chenguangjun <chen.gj@qq.com>
2019-10-14 16:48:23 +0800

    fix

--- app/WebSocket/Middleware/UserAuth.php
+++ app/WebSocket/Middleware/UserAuth.php
@@ -12,8 +12,7 @@ class UserAuth extends Middleware
     public function handle($io){
         $fd = $io->getInputFd();
         if(!ChatUser::getUser($fd)){
-            $io->e('未登录');
-            return false;
+            return $io->e('未登录');
         }
 
         return true;
